aced4e04a3d1170262166dfc6ed58b783fe694a2
Alex Koumpas <akoumpas@blockchain.com>
2020-04-29 20:09:34 +0100

    organising codebase

--- scripts/script_place_orders.py
+++ scripts/script_place_orders.py
@@ -1,8 +1,9 @@
 import time
 import json
 import asyncio
+import pdb
 import configparser
-from blockchain_exchange_client import BlockchainWebsocketClient
+from src.blockchain_exchange_client import BlockchainWebsocketClient
 
 
 def derive_keys_from_config(env):
@@ -34,6 +35,7 @@ async def process_responses():
         elif (res['channel'] == 'symbols' and res['event'] == 'updated'):
             idx = res['base_currency'] + '-' + res['counter_currency']
             symbols_store[idx] = res
+            # pdb.set_trace()
 
     def handle_prices(res, prices_store):
         if (res['channel'] == 'ticker'):
@@ -65,6 +67,48 @@ async def process_responses():
             else:
                 Exception('Unknown event type. Exiting')
 
+    # Market-orders
+    def place_market_order(res, sent_order, price_store, symbols_store):
+        clOrdId = ''
+        if (mode == "market_orders" and sent_order == False and symbols_store != {} and symbols_store[
+            symbol] != {}):
+            orderQty_multiplier = 2.0  # how many times more than the minimum size we want to quote
+            orderQty = (symbols_store[symbol]['min_order_size'] * 10 ** (
+                -symbols_store[symbol]['min_order_size_scale'])) * orderQty_multiplier
+            clOrdId = client.create_new_market_order(side='buy', orderQty=orderQty, price=price_store[symbol])
+            sent_order = True
+        return clOrdId, sent_order
+
+    # Limit-orders
+    def place_limit_order(res, sent_order, prices_store, symbols_store):
+        clOrdId = ''
+        if (mode == "limit_orders" and sent_order == False and symbols_store != {} and prices_store != {}):
+            # pdb.set_trace(
+            print(symbols_store['BTC-USD'])
+            try:
+                if symbols_store['BTC-USD'] != None and prices_store['BTC-USD'] != None:
+                    orderQty_multiplier = 2.0  # how many times more than the minimum size we want to quote
+                    orderQty = (symbols_store[symbol]['min_order_size'] * 10 ** (
+                        -symbols_store[symbol]['min_order_size_scale'])) * orderQty_multiplier
+                    clOrdId = client.create_new_limit_order(side='buy', orderQty=orderQty, price=prices_store[symbol],
+                                                 symbol=symbols_store[symbol])
+                    sent_order = True
+            except:
+                print("Cannot find symbol " + symbol)
+                pdb.set_trace()
+        return clOrdId, sent_order
+
+    def cancel_placed_order(res, clOrdId, sent_order, orders_store):
+        if (mode == "limit_orders" and sent_order == True):
+            time.sleep(5)
+            try:
+                client.cancel_order(lookup_orderID(clOrdId, orders_store))
+                pdb.set_trace()
+                exit()
+            except:
+                print('Order not accepted yet')
+        return sent_order, clOrdId
+
     symbols_store = {}
     prices_store = {}
     balances_store = {}
@@ -78,9 +122,15 @@ async def process_responses():
         print(res)
         handle_symbols(res)
         handle_balances(res)
+        await asyncio.sleep(0.2)
         handle_tob(res)
         prices_store = handle_prices(res, prices_store)
         orders_store = handle_order_updates(res)
+        clOrdId, sent_order = place_market_order(res, sent_order, prices_store, symbols_store)
+        if prices_store != None and symbols_store != None and balances_store != None:
+            clOrdId, sent_order = place_limit_order(res, sent_order, prices_store, symbols_store)
+        cancel_placed_order(res, clOrdId, sent_order, orders_store)
+        await asyncio.sleep(0.1)
         # client.ws.close()
 
 
